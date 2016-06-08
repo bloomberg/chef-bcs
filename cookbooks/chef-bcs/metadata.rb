4ca1beabc382dfee36b20d37ddaa3acd6ced52f0
cloudm2 <cjones@cloudm2.com>
2016-06-08 12:53:30 -0400

    Updated metadata

--- cookbooks/chef-bcs/metadata.rb
+++ cookbooks/chef-bcs/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cjones303@bloomberg.net'
 license 'Apache v2.0'
 description 'Installs/Configures chef-bcs'
 long_description 'Installs/Configures chef-bcs - requires github.com/ceph/ceph-chef'
-version '10.2.0'
+version '10.2.1'
 
 depends "chef-client", ">= 2.2.2"
 depends "cron", ">= 1.7.4"
