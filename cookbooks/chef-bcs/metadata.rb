0ddba3627d3d5758d852ab707dba1593f4797784
cloudm2 <cjones@cloudm2.com>
2016-06-23 13:32:44 -0400

    Updated version

--- cookbooks/chef-bcs/metadata.rb
+++ cookbooks/chef-bcs/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'cjones303@bloomberg.net'
 license 'Apache v2.0'
 description 'Installs/Configures chef-bcs'
 long_description 'Installs/Configures chef-bcs - requires github.com/ceph/ceph-chef'
-version '10.2.4'
+version '10.2.5'
 
 depends "chef-client", ">= 2.2.2"
-depends "cron", ">= 1.7.4"
+depends "cron", ">= 1.7.6"
 depends "ntp", ">= 1.10.0"
 depends "ceph-chef", ">= 0.9.3"
 # depends "firewall", ">= 2.4"
@@ -15,8 +15,8 @@ depends "sudo", ">= 2.7.2"
 depends "collectd", ">= 2.2.2"
 depends "collectd_plugins", ">= 2.1.1"
 
-supports 'redhat', '>= 7.1'
-supports 'centos', '>= 7.1'
+supports 'redhat', '>= 7.2'
+supports 'centos', '>= 7.2'
 
 issues_url 'https://github.com/bloomberg/chef-bcs/issues'
 source_url 'https://github.com/bloomberg/chef-bcs'
