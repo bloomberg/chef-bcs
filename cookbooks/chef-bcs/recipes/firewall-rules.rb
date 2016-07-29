83d0f44f1b901dfca913c1905522911f92984989
cloudm2 <cjones@cloudm2.com>
2016-07-29 13:38:36 -0400

    Added additional firewall options

--- cookbooks/chef-bcs/recipes/firewall-rules.rb
+++ cookbooks/chef-bcs/recipes/firewall-rules.rb
@@ -46,100 +46,35 @@
 bash 'set-default-zone' do
   user 'root'
   code <<-EOH
-    firewall-cmd --set-default-zone=public
+    firewall-cmd --set-default-zone=#{node['chef-bcs']['security']['firewall']['zone']}
   EOH
 end
 
-node['chef-bcs']['security']['firewall']['interfaces'].each do | interface |
-  # Build port list
-  unique_ports = []
-  ports = []
-  ranges = []
-
-  interface['ports'].each do | port |
-    if node.tags.include? port['role']
-      if port['open'].any?
-        ports << port['open']
+# IMPORTANT: Make sure to include SSH rule in the rules...
+if node['chef-bcs']['security']['firewall']['use'] == 'rules'
+  node['chef-bcs']['security']['firewall']['rules'].each do | rule |
+    cmd = "firewall-cmd --zone=#{rule['zone']} "
+    if rule['permanent']
+      cmd += "--permanent "
+    end
+    rule['rules'].each do | item_rule |
+      if rule['type'] == 'rich-rule'
+        cmd_tmp = cmd + "#{item_rule}"
+      else
+        # assumes service
+        cmd_tmp = cmd + "--add-service='#{item_rule}'"
       end
-
-      # Ranges are ALL tcp types
-      port['ranges'].each do | range |
-        if range['start'] > 0
-          ranges << range
-          #start_range = range['start']
-          #end_range = range['end']
-          #ranges << (start_range..end_range)
-        end
-      end
+      cmd_output = shell_out(cmd_tmp)
+      puts cmd_tmp + " -- " + cmd_output
     end
   end
-
-  if ports.any?
-    unique_ports = ports.flatten.uniq
-  end
-
-  unique_ports.each do | uport |
-#    firewall_rule interface['name'] do
-#      interface "#{node['chef-bcs']['network'][interface['name']]['interface']}"
-#      port uport['port']
-#      protocol uport['protocol']
-#      permanent true
-#      command :allow
-#    end
-
-    #protocol = 'tcp'
-    #if uport['protocol'] == 17
-    #  protocol = 'udp'
-    #end
-
-    cmd = shell_out("firewall-cmd --permanent --add-port=#{uport['port']}/#{uport['protocol']}")
-    puts "Port: #{uport['port']}/#{uport['protocol']} " + cmd.stdout
-  end
-
-  ranges.each do | range |
-#    firewall_rule interface['name'] do
-#      interface "#{node['chef-bcs']['network'][interface['name']]['interface']}"
-#      port range
-#      permanent true
-#      command :allow
-#    end
-    #protocol = 'tcp'
-    #if range['protocol'] == 17
-    #  protocol = 'udp'
-    #end
-
-    cmd = shell_out("firewall-cmd --permanent --add-port=#{range['start']}-#{range['end']}/#{range['protocol']}")
-    puts "Range: #{range['start']}-#{range['end']}/#{range['protocol']} " + cmd.stdout
-
-  end
+else
+  include_recipe 'chef-bcs::firewall-rules-interfaces'
 end
 
-node['chef-bcs']['network']['public']['cidr'].each do | cidr |
-  cmd = shell_out("firewall-cmd --permanent --zone=internal --add-source=#{cidr}")
-  puts "Adding 'internal' source IP range: #{cidr} " + cmd.stdout
-end
-
-node['chef-bcs']['network']['cluster']['cidr'].each do | cidr |
-  cmd = shell_out("firewall-cmd --permanent --zone=internal --add-source=#{cidr}")
-  puts "Adding 'internal' source IP range: #{cidr} " + cmd.stdout
-end
-
-# NOTE: These rules should ALWAYS be present.
-# firewall_rule 'ssh' do
-#   port     22
-#   command  :allow
-# end
-
-# Force the rules etc to be saved
-# firewall 'default' do
-#   action :save
-# end
-
-bash 'ssh-and-reload' do
+bash 'firewall-reload' do
   user 'root'
   code <<-EOH
-    firewall-cmd --permanent --zone=public --add-port=22/tcp
-    firewall-cmd --permanent --zone=internal --add-port=22/tcp
     firewall-cmd --reload
   EOH
 end
--- cookbooks/chef-bcs/recipes/firewall-start.rb
+++ cookbooks/chef-bcs/recipes/firewall-start.rb
@@ -19,7 +19,11 @@
 
 # NOTE: Not started by default. Must execute the recipe or include it in a role list.
 
-execute 'firewalld-start' do
-  command 'sudo systemctl start firewalld'
-  only_if "sudo systemctl status firewalld | grep dead"
+if node['chef-bcs']['security']['firewall']['enable']
+  execute 'firewalld-start' do
+    command 'sudo systemctl start firewalld'
+    only_if "sudo systemctl status firewalld | grep dead"
+  end
+else
+  include_recipe 'chef-bcs::firewalld-stop'
 end
