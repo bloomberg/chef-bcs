17bfc5b5798d48b78245ff77e56a7b7f8150be4a
hasncj1 <cjones@cloudm2.com>
2017-03-17 14:49:05 -0400

    Added perf

--- cookbooks/chef-bcs/recipes/default.rb
+++ cookbooks/chef-bcs/recipes/default.rb
@@ -41,6 +41,9 @@ package 'fping'
 # Used to help find sensor issues on nodes if BMC doesn't show much
 package 'lm_sensors'
 
+# Helps with checking OSD performance 
+package 'perf'
+
 # I/O troubleshooting tools
 package 'fio'
 package 'bc'
